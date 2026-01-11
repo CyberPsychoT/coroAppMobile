31ef0c209340eeb50733d41d5b3fa2acb21fc179
Raúl Mariscal <mariscaldev@gmx.com>
2026-01-11 18:24:06 -0300

    Version 108

--- src/app/version.ts
+++ src/app/version.ts
@@ -1,4 +1,4 @@
 export const APP_CONFIG = {
-  version: '1.0.7',
-  build_number: 107 // Increment this manually before each release
+  version: '1.0.8',
+  build_number: 108 // Increment this manually before each release
 };
